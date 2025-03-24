f9847e867cd86fc99e70fdd5f40cc2703799a7f6
CJay-Cipher <chijiokennabueze@gmail.com>
2025-03-24 12:26:17 +0100

    sigm up page update

--- src/app/register/SignUpForm.tsx
+++ src/app/register/SignUpForm.tsx
@@ -2,58 +2,64 @@ import React from "react";
 
 const SignUpForm = () => {
   return (
-    <div>
-      <form action="" className="flex flex-col items-center">
-        <div className="w-full flex flex-col items-center sm:gap-6 gap-4">
-          <div className="md:max-w-[400px] max-w-[350px] w-full">
-            <label htmlFor=""></label>
-            <input
-              type="text"
-              className="w-full lg:h-[50px] 2xs:h-[45px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] lg:rounded-[15px] rounded-[10px] border border-custom-gray-dark"
-              placeholder="First name"
-            />
-          </div>
-          <div className="md:max-w-[400px] max-w-[350px] w-full">
-            <label htmlFor=""></label>
-            <input
-              type="text"
-              className="w-full lg:h-[50px] 2xs:h-[45px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] lg:rounded-[15px] rounded-[10px] border border-custom-gray-dark"
-              placeholder="Last name"
-            />
-          </div>
-          <div className="md:max-w-[400px] max-w-[350px] w-full">
-            <label htmlFor=""></label>
-            <input
-              type="text"
-              className="w-full lg:h-[50px] 2xs:h-[45px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] lg:rounded-[15px] rounded-[10px] border border-custom-gray-dark"
-              placeholder="Email"
-            />
-          </div>
-          <div className="md:max-w-[400px] max-w-[350px] w-full">
-            <label htmlFor=""></label>
-            <input
-              type="text"
-              className="w-full lg:h-[50px] 2xs:h-[45px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] lg:rounded-[15px] rounded-[10px] border border-custom-gray-dark"
-              placeholder="Password"
-            />
-          </div>
-          <div className="md:max-w-[400px] max-w-[350px] w-full">
-            <label htmlFor=""></label>
-            <input
-              type="text"
-              className="w-full lg:h-[50px] 2xs:h-[45px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] lg:rounded-[15px] rounded-[10px] border border-custom-gray-dark"
-              placeholder="Confirm password"
-            />
-          </div>
+    <div className="flex justify-center items-center min-h-max w-full z-10 md:pt-6 p-4">
+      <div className="bg-main-white w-full xl:max-w-[450px] md:max-w-[400px] max-w-[350px] xl:py-8 lg:py-6 p-4 border border-shadow lg:rounded-[20px] rounded-[15px] shadow-xl">
+        <div className="text-center xl:pb-10 md:pb-8 pb-6">
+          <h1 className="lg:text-[36px] md:text-[30px] text-[26px] text-button-bg font-bold leading-[1.2] tracking-tight">Sign Up</h1>
+          <p className="md:text-[14px] text-[12px] text-custom-gray pl-[2px]">Register to see amazing deals</p>
         </div>
+        <form action="" className="flex flex-col items-center">
+          <div className="w-full flex flex-col items-center xl:gap-4 md:gap-3 gap-2">
+            <div className="xl:max-w-[400px] md:max-w-[350px] max-w-[320px] w-full">
+              <label htmlFor=""></label>
+              <input
+                type="text"
+                className="w-full xl:h-[50px] md:h-[45px] 2xs:h-[40px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] xl:rounded-[15px] md:rounded-[10px] rounded-[20px] border border-custom-gray-dark"
+                placeholder="First name"
+              />
+            </div>
+            <div className="xl:max-w-[400px] md:max-w-[350px] max-w-[320px] w-full">
+              <label htmlFor=""></label>
+              <input
+                type="text"
+                className="w-full xl:h-[50px] md:h-[45px] 2xs:h-[40px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] xl:rounded-[15px] md:rounded-[10px] rounded-[20px] border border-custom-gray-dark"
+                placeholder="Last name"
+              />
+            </div>
+            <div className="xl:max-w-[400px] md:max-w-[350px] max-w-[320px] w-full">
+              <label htmlFor=""></label>
+              <input
+                type="text"
+                className="w-full xl:h-[50px] md:h-[45px] 2xs:h-[40px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] xl:rounded-[15px] md:rounded-[10px] rounded-[20px] border border-custom-gray-dark"
+                placeholder="Email"
+              />
+            </div>
+            <div className="xl:max-w-[400px] md:max-w-[350px] max-w-[320px] w-full">
+              <label htmlFor=""></label>
+              <input
+                type="text"
+                className="w-full xl:h-[50px] md:h-[45px] 2xs:h-[40px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] xl:rounded-[15px] md:rounded-[10px] rounded-[20px] border border-custom-gray-dark"
+                placeholder="Password"
+              />
+            </div>
+            <div className="xl:max-w-[400px] md:max-w-[350px] max-w-[320px] w-full">
+              <label htmlFor=""></label>
+              <input
+                type="text"
+                className="w-full xl:h-[50px] md:h-[45px] 2xs:h-[40px] h-[40px] xs:p-4 p-3 lg:text-[16px] md:text-[14px] text-[12px] xl:rounded-[15px] md:rounded-[10px] rounded-[20px] border border-custom-gray-dark"
+                placeholder="Confirm password"
+              />
+            </div>
+          </div>
 
-        <button className="md:max-w-[400px] max-w-[350px] w-full lg:h-[50px] 2xs:h-[45px] h-[40px] md:mt-8 mt-6 lg:text-[20px] md:text-[18px] text-[16px] text-main-white font-medium bg-custom-blue-2 hover:bg-custom-blue lg:rounded-[15px] rounded-[10px] border border-custom-gray-dark">
-          Sign Up
-        </button>
-        <p className="md:m-2 m-1 lg:text-[14px] text-[12px] text-center">
-          Already have an account? <span className="font-semibold text-custom-blue-2 hover:text-custom-blue cursor-pointer">Login</span>
-        </p>
-      </form>
+          <button className="xl:max-w-[400px] md:max-w-[350px] max-w-[320px] w-full xl:h-[50px] md:h-[45px] 2xs:h-[40px] h-[40px] md:mt-8 mt-6 lg:text-[20px] md:text-[18px] text-[16px] text-main-white font-medium bg-custom-blue-2 hover:bg-custom-blue xl:rounded-[15px] md:rounded-[10px] rounded-[20px] border border-custom-gray-dark">
+            Create Account
+          </button>
+          <p className="md:m-2 m-1 lg:text-[14px] text-[12px] text-center">
+            Already have an account? <span className="font-semibold text-custom-blue-2 hover:text-custom-blue cursor-pointer">Login</span>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,41 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import signUpImage from "../../images/signUpImage.png";
-import abstractBg from "../../images/waveBg.png";
+import gridBg from "../../images/bg-grid-lighter.svg";
 import SignUpForm from "./SignUpForm";
 
 const Register = () => {
   return (
-    <div className="font-montserrat flex justify-center items-center w-full h-max min-h-screen">
-      <div className="w-full flex">
-        <div className="relative max-lg:hidden overflow-hidden">
-          <Image width={200} height={200} src={signUpImage} alt="side image" className="h-max min-h-screen w-[600px] object-center object-cover" />
-          <div className="absolute top-0 flex flex-col justify-center h-full z-10 xl:px-10 px-8">
-            <h1 className="xl:text-[40px] lg:text-[34px] text-[28px] text-main-white font-bold leading-[1.2] tracking-tight">
-              Explore the Best Deals on Premium Fragrances
+    <div className="font-montserrat h-screen flex justify-center items-center">
+      <div className="h-screen max-h-[1200px] w-full max-w-[2000px] flex mx-auto shadow-2xl">
+        <div className="relative h-full max-md:hidden w-[40%] overflow-hidden">
+          <Image width={200} height={200} src={signUpImage} alt="side image" className="w-full h-full object-center object-cover" />
+          <div className="absolute top-0 flex flex-col justify-center h-full z-10 xl:p-12 lg:p-8 md:p-10 p-8">
+            <h1 className="xl:text-[44px] lg:text-[36px] md:text-[34px] text-[28px] text-main-white font-bold leading-[1.2] tracking-tight">
+              Explore the Best <br /> Deals on Premium Fragrances
             </h1>
             <p className="xl:text-[16px] lg:text-[14px] text-[12px] text-main-white pl-[2px]">Discover The Best Deals on top Fragrances</p>
           </div>
         </div>
-        <div className="relative overflow-y-auto">
-          <Image
-            width={200}
-            height={200}
-            src={abstractBg}
-            alt="abstract"
-            className="h-max md:max-h-screen min-h-screen lg:w-[70vw] w-screen object-center object-cover"
-          />
-          <div className="absolute top-0 left-0 flex justify-center items-center h-full w-full z-10">
-            <div className="md:max-w-[500px] max-w-[400px] w-full bg-main-white md:m-6 m-4 md:py-10 py-8 p-4 border-shadow lg:rounded-[20px] rounded-[15px] shadow-xl ">
-              <div className="text-center md:pb-10 pb-8">
-                <h1 className="lg:text-[36px] md:text-[30px] text-[26px] text-button-bg font-bold leading-[1.2] tracking-tight">Sign Up</h1>
-                <p className="md:text-[14px] text-[12px] text-custom-gray pl-[2px]">Register to see amazing deals</p>
-              </div>
-              <SignUpForm />
-            </div>
-          </div>
+        <div className="relative flex flex-1 overflow-y-auto bg-linear-to-b from-custom-red-light to-shadow">
+          <Image width={200} height={200} src={gridBg} alt="abstract" className="absolute w-full h-full object-center object-cover" />
+          <SignUpForm />
         </div>
-        <div></div>
       </div>
     </div>
   );
