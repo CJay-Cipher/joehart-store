368f0c045951e7047f093183eebdd986379b6b3a
CJay-Cipher <chijiokennabueze@gmail.com>
2025-03-25 17:25:05 +0100

    update

--- src/components/AuthPageContent.tsx
+++ src/components/AuthPageContent.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 
 type AuthPageContentProps = {
-  headerText: string;
-  subText: string;
+  headerText?: string;
+  subText?: string;
   hideContent?: boolean;
+  isDefault?: boolean;
 };
-const AuthPageContent = ({ headerText, subText, hideContent }: AuthPageContentProps) => {
+const AuthPageContent = ({ headerText, subText, hideContent, isDefault }: AuthPageContentProps) => {
   return (
     <div
-      className={`md:absolute top-0 flex flex-col justify-center max-md:max-w-[500px] max-xs:max-w-[300px] md:h-full z-10 xl:p-8 lg:p-6 md:p-6 p-4 ${
-        hideContent && "md:hidden"
-      }`}
+      className={` ${
+        !isDefault ? "md:absolute top-0 flex flex-col justify-center md:h-full max-md:text-button-bg text-main-white" : "max-w-[300px] text-main-black text-center"
+      } xl:max-w-[450px] md:max-w-[400px] max-w-[350px] z-10 xl:p-8 lg:p-6 md:p-6 p-4 ${hideContent && !isDefault && "md:hidden"}`}
     >
-      <h1 className="xl:text-[40px] lg:text-[30px] md:text-[24px] 2xs:text-[23px] text-[19px] max-md:text-center max-md:text-button-bg text-main-white font-bold leading-[1.2] tracking-tight">
-        {headerText}
-      </h1>
-      <p className="lg:mt-2 mt-1 xl:text-[16px] lg:text-[14px] text-[12px] max-md:text-center max-md:text-button-bg text-main-white pl-[2px]">{subText}</p>
+      <h1 className="xl:text-[40px] lg:text-[30px] md:text-[24px] 2xs:text-[23px] text-[19px] max-md:text-center font-bold leading-[1.2] tracking-tight">{headerText}</h1>
+      <p className="lg:mt-2 mt-1 xl:text-[16px] lg:text-[14px] text-[12px] max-md:text-center pl-[2px]">{subText}</p>
     </div>
   );
 };
