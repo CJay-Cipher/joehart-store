b711dbc23a7bc4dc397227b63eaaceed6038e9b9
CJay-Cipher <chijiokennabueze@gmail.com>
2025-03-28 04:19:11 +0100

    font family update on auth pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,7 @@ import AuthPageContent from "../../components/AuthPageContent";
 
 const Login = () => {
   return (
-    <div className="font-outfit h-[100svh] flex justify-center items-center">
+    <div className="font-montserrat h-[100svh] flex justify-center items-center">
       <div className="h-[100svh] max-h-[1500px] w-full max-w-[2000px] flex mx-auto shadow-2xl">
         <div className="relative h-full max-md:hidden w-[40%] overflow-hidden">
           <Image quality={100} src={loginImage} alt="side image" className="w-full h-full object-center object-cover" />
@@ -19,10 +19,10 @@ const Login = () => {
           <Image quality={100} src={gridBg} alt="abstract" className="absolute w-full h-full object-center object-cover" />
           <Link
             href="/"
-            className="z-50 self-start font-outfit lg:h-12 h-8  lg:p-4 p-2 flex justify-center items-center gap-[3px] xl:text-[18px] text-[16px] text-nowrap font-bold text-main-black tracking-[0.9px] cursor-pointer"
+            className="z-50 self-start lg:h-12 h-8  lg:p-4 p-2 flex justify-center items-center gap-[3px] xl:text-[18px] text-[16px] text-nowrap font-bold text-main-black tracking-[0.9px] cursor-pointer"
           >
             <Image src={perfume} alt="Perfume" className="xl:w-[26px] xl:h-[26px] md:w-[24px] md:h-[24px] w-[20px] h-[20px]" />
-            <span className="translate-y-[0.8px] xl:text-[18px] md:text-[16px] text-[14px]">JoeHart </span>
+            <span className="font-outfit translate-y-[0.8px] xl:text-[18px] md:text-[16px] text-[14px]">JoeHart </span>
           </Link>
           <div className="z-20 flex-1 w-full flex justify-center items-center flex-col ">
             <AuthPageContent
