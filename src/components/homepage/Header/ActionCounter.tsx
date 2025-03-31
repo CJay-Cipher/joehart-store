bfbcafb723c19e4bd6d22c9803ccfa7bdd4b86fc
CJay-Cipher <chijiokennabueze@gmail.com>
2025-03-31 17:01:12 +0100

    header active counter update

--- src/components/homepage/Header/ActionCounter.tsx
+++ src/components/homepage/Header/ActionCounter.tsx
@@ -11,7 +11,7 @@ const ActionCounter = ({ href, label, Icon, counter }: ActionCounterProps) => (
     href={href}
     className="relative flex items-center gap-[2px] xl:text-[12px] text-[11px] text-custom-gray-darker text-nowrap font-medium rounded-[50px] border border-transparent transition-color duration-200 hover:text-main-black md:hover:scale-[1.1] hover:scale-[1.15]"
   >
-    <span className="max-md:hidden">{label}</span>
+    <span className="">{label}</span>
     <Icon className="h-[18px] w-[18px]" />
     {counter > 0 && (
       <span className="absolute flex justify-center items-center md:h-[18px] md:w-[18px] h-[15px] w-[15px] md:top-[-12px] top-[-10px] right-[-8px] md:text-[12px] text-[10px] text-main-white bg-custom-red font-semibold rounded-[50%]">
