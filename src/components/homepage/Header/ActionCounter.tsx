a1751486c209afa207eebecf7e0704bf260c29c2
CJay-Cipher <chijiokennabueze@gmail.com>
2025-03-31 18:06:51 +0100

    header update

--- src/components/homepage/Header/ActionCounter.tsx
+++ src/components/homepage/Header/ActionCounter.tsx
@@ -9,12 +9,12 @@ type ActionCounterProps = {
 const ActionCounter = ({ href, label, Icon, counter }: ActionCounterProps) => (
   <Link
     href={href}
-    className="relative flex items-center gap-[3px] px-[14px] py-[5px] xl:text-[12px] text-[11px] text-main-black text-nowrap font-medium lg:rounded-[30px] xs:rounded-[20px] rounded-[5px] bg-main-white transition-color duration-200 hover:text-main-white hover:bg-main-black"
+    className="relative flex items-center gap-[3px] xs:px-[14px] xs:py-[5px] max-xs:p-[6px] xl:text-[12px] text-[11px] text-main-black text-nowrap font-medium border border-custom-slate-400 lg:rounded-[30px] xs:rounded-[20px] rounded-[50%] bg-main-white transition-color duration-200 hover:text-main-white hover:bg-main-black"
   >
     <span className="max-xs:hidden">{label}</span>
-    <Icon className="h-[18px] w-[18px]" />
+    <Icon className="xs:h-[18px] xs:w-[18px] h-[21px] w-[21px]" />
     {counter > 0 && (
-      <span className="absolute flex justify-center items-center md:h-[18px] md:w-[18px] h-[15px] w-[15px] md:top-[-12px] top-[-8px] right-[-5px] md:text-[12px] text-[10px] text-main-white bg-custom-red font-semibold rounded-[50%]">
+      <span className="absolute flex justify-center items-center md:h-[18px] md:w-[18px] h-[15px] w-[15px] md:top-[-12px] top-[-6px] right-[-4px] md:text-[12px] text-[10px] text-main-white bg-custom-red font-semibold rounded-[50%]">
         {counter}
       </span>
     )}
