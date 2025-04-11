40c268723741f2f92994742d2360b6ee8dadbb06
CJay-Cipher <chijiokennabueze@gmail.com>
2025-04-11 23:43:30 +0100

    add cloudinary image url to config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: "", // Leave empty if not using a specific port
         pathname: "/**", // Allows all paths; adjust if necessary
       },
+      {
+        protocol: "https", // Use 'http' or 'https' as needed
+        hostname: "res.cloudinary.com", // Your external image domain
+        port: "", // Leave empty if not using a specific port
+        pathname: "/**", // Allows all paths; adjust if necessary
+      },
     ],
   },
   // Other configuration options can go here
